aab970b8e2fb0a8967ee43e29583ba506c8af283
Minki Kim <minki0415@naver.com>
2022-03-30 20:35:55 +0900

    Design: Update chart design

--- front/src/components/Stock/Stock.jsx
+++ front/src/components/Stock/Stock.jsx
@@ -4,19 +4,22 @@ import classes from './Stock.module.css'
 const Stock = (props) => {
 
   return (
-        <div className={classes.chart__list__one}>
-            <div className={classes.chart__list__one__header}>
-              <h1>{props.name}</h1>
-              <h2>{props.price}</h2>
-            </div>
-            <div className={classes.chart__list__one__body}>
-              <h3>{props.change}</h3>
-              <h3>{props.changeRate}</h3>
-            </div>
-            <div className={classes.chart__list__one__chart}>
-              <img src={props.chartUrl} alt="" />
+      <div className={classes.stock_data}>
+          <div className={classes.stock_data__header}>
+            <h1>{props.name}</h1>
+            <div className={classes.stock_data__header_sub}>
+                <h2>{props.price}</h2>
+                <h4>{props.changeRate}</h4>
+                <h4>{props.change}</h4>
             </div>
           </div>
+          {/* <div className={classes.stock_data__body}>
+            <h3>{props.change}</h3>
+          </div> */}
+          <div className={classes.stock_data__chart}>
+            <img src={props.chartUrl} alt="" />
+          </div>
+      </div>
 
   )
 }
