1290c86a8ac195f33abfdd4cccebfbcb11e2a1b8
mandariny <mandariny716@gmail.com>
2022-05-15 16:44:12 +0900

    Refactor: Add @Builder to WatchStockDTO
    Add @Builder to WatchStockDTO
    
    Related to: #23

--- back/src/main/java/dev/pomyharry/stocksimulator/back/model/dto/CustomerDTO.java
+++ back/src/main/java/dev/pomyharry/stocksimulator/back/model/dto/CustomerDTO.java
@@ -1,9 +1,11 @@
 package dev.pomyharry.stocksimulator.back.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
