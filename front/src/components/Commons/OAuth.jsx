5ef84bf770866dac92941097a8f09dc9680f16b4
dohee789 <dojin3773@gmail.com>
2022-06-16 10:03:54 +0900

    OAuth 추가

---
+++ front/src/components/Commons/OAuth.jsx
@@ -0,0 +1,4 @@
+export const CLIENT_ID  = "ef23cfa2f66fcc25924f54647f3f5460";
+export const REDIRECT_URI =  "http://localhost:3000/oauth/kakao";
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
