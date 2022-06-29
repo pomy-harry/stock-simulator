07ed0a374762f948afeb13c021af6cd587984cc1
dohee789 <dojin3773@gmail.com>
2022-06-29 23:29:36 +0900

    refactor:kakaologin

--- back/src/main/java/dev/pomyharry/stocksimulator/back/service/KakaoService.java
+++ back/src/main/java/dev/pomyharry/stocksimulator/back/service/KakaoService.java
@@ -1,13 +1,14 @@
 package dev.pomyharry.stocksimulator.back.service;
 
 import dev.pomyharry.stocksimulator.back.model.dto.KakaoDTO;
-import dev.pomyharry.stocksimulator.back.model.entity.Kakao;
 
 public interface KakaoService {
 
-	KakaoDTO getUserInfo(String access_Token);
+	KakaoDTO kakaoJoin(String access_Token);
 
-	Kakao findById(String id);
+	void kakaoLogin(String access_Token);
 
 	String getAccessToken (String authorize_code);
+
+	KakaoDTO getUserInfo(String access_Token);
 }
--- back/src/main/java/dev/pomyharry/stocksimulator/back/service/KakaoServiceImpl.java
+++ back/src/main/java/dev/pomyharry/stocksimulator/back/service/KakaoServiceImpl.java
@@ -9,6 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import dev.pomyharry.stocksimulator.back.exception.IdNotFoundException;
 import dev.pomyharry.stocksimulator.back.model.dto.KakaoDTO;
 import dev.pomyharry.stocksimulator.back.model.entity.Customer;
 import dev.pomyharry.stocksimulator.back.model.entity.Kakao;
@@ -82,6 +82,7 @@ public class KakaoServiceImpl implements KakaoService {
 			System.out.println("access_token : " + access_Token);
 			System.out.println("refresh_token : " + refresh_Token);
             
+
 			br.close();
 			bw.close();
 		} catch (IOException e) {
@@ -91,75 +92,108 @@ public class KakaoServiceImpl implements KakaoService {
 	}
 
 	@Override
-	public Kakao findById(String id) {
-		Kakao kakao = kakaoRepository.findByEmail(id);
-		if (kakao == null ) {
-            throw new IdNotFoundException("Id cannot be found");
-        } 
-        return kakao;
-	}
-	
+	public KakaoDTO getUserInfo(String access_Token){
+		
+		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+		 HashMap<String, Object> userInfo = new HashMap<String, Object>();
+
+		 String reqURL = "https://kapi.kakao.com/v2/user/me";
+		 try {
+			 URL url = new URL(reqURL);
+			 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			 conn.setRequestMethod("GET");
+	 
+			 // 요청에 필요한 Header에 포함될 내용
+			 conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	 
+			 int responseCode = conn.getResponseCode();
+			 System.out.println("responseCode : " + responseCode);
+	 
+			 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+	 
+			 String line = "";
+			 String result = "";
+	 
+			 while ((line = br.readLine()) != null) {
+				 result += line;
+			 }
+			 br.close();
+			 System.out.println("response body : " + result);
+			 conn.disconnect();
+
+			 JsonParser parser = new JsonParser();
+			 JsonElement element = parser.parse(result);
+	 
+			 JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+			 JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+			 
+			 String user_id = element.getAsJsonObject().get("id").getAsString();
+			 String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+			 String email = kakao_account.getAsJsonObject().get("email").getAsString();
+ 
+ 
+			 userInfo.put("id", user_id);
+			 userInfo.put("nickname", nickname);
+			 userInfo.put("email", email);
+		
+		
+			 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			//return kakaoRepository.findByEmail(userInfo);
+			return new KakaoDTO(userInfo.get("id").toString(), userInfo.get("nickname").toString(), userInfo.get("email").toString());
+
+    }
 
 	@Override
-    public KakaoDTO getUserInfo(String access_Token) {
+    public void kakaoLogin(String access_Token) {
 
-        // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
-        HashMap<String, Object> userInfo = new HashMap<String, Object>();
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-    
-            // 요청에 필요한 Header에 포함될 내용
-            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-    
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-    
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-    
-            String line = "";
-            String result = "";
-    
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-    
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-    
-            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-			String user_id = element.getAsJsonObject().get("id").getAsString();
-    
-            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
-            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+		// 1. email을가지고 디비에 조회를한다.
+		Kakao succLogin = kakaoRepository.findByEmail(getUserInfo(access_Token).getEmail());
+
+		// 2-1 email이있다 ? 로그인시킨다
+		// 2-2 email이없다 ? 회원가입 하라는 알림창
+		// if(succLogin == null){
+		// 	throw new NeedKakaoJoinException(errMsg:"회원가입을 진행해주세요");
+		// }
+		if (succLogin != null || succLogin.getEmail() == getUserInfo(access_Token).getEmail() ) {
+			System.out.println(succLogin.getEmail() +"/////");
+			System.out.println(getUserInfo(access_Token).getEmail()+"///");
+
+			Customer customer = new Customer();
+			customer.setId(succLogin.getId());
+			customer.setName(succLogin.getName());
+			customer.setEmail(succLogin.getEmail());
+
+			customerRepository.save(customer);	
+			System.out.println("들어갔니?"+ customer);
+		}
+		
+		
+    }
 
 
-			userInfo.put("id", user_id);
-            userInfo.put("nickname", nickname);
-            userInfo.put("email", email);
-			
-			Kakao succLogin = kakaoRepository.findByEmail(email);
+	@Override
+    public KakaoDTO kakaoJoin(String access_Token) {
+
+		// 1. email을가지고 디비에 조회를한다.
+		Kakao succLogin = kakaoRepository.findByEmail(getUserInfo(access_Token).getEmail());
 			System.out.println(succLogin);
-			if (succLogin == null) {
 
-				// 1. 아이디를가지고 디비에 조회를한다.
-				// 2-1 아이디가 있다 ? 로그인시킨다
-				// 2-2 아이디가없다 ? 디비에 값을 인서트 시키고, 로그인시킨다.
-				
+			// 2-1 email이없다 ? 디비에 값을 인서트 시킴
+			// 2-1 email이있다 ? 로그인 하라는 알림창
+			if (succLogin == null) {				
 				// customer에 id 저장후 
 				Customer customer = new Customer();
-				customer.setId(user_id);
-				customer.setName(nickname);
-				customer.setEmail(email);
+				customer.setId(getUserInfo(access_Token).getId());
+				customer.setName(getUserInfo(access_Token).getName());
+				customer.setEmail(getUserInfo(access_Token).getEmail());
 
 				customerRepository.save(customer);				
 
 				// email로 조회
-				Customer c = customerRepository.findByEmail(email);
+				Customer c = customerRepository.findByEmail("email");
 
 				// kakao에도 가져온 id 넣기
 				Kakao kakao = new Kakao();
@@ -169,19 +203,15 @@ public class KakaoServiceImpl implements KakaoService {
 
 				kakaoRepository.save(kakao);
 				
+				
+
+			}else if(succLogin != null & succLogin.getEmail() == getUserInfo(access_Token).getEmail()){
+				System.out.println("중복된 이메일입니다. 로그인 해주세요");
 			}
-			// kakao로 조회
-			Kakao searchInfo = kakaoRepository.findByEmail(email);
+		// kakao로 조회
+		Kakao searchInfo = kakaoRepository.findByEmail(getUserInfo(access_Token).getEmail());
 
-			return new KakaoDTO(searchInfo.getId(), searchInfo.getName(), searchInfo.getEmail());
-
-    
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-		return kakaoRepository.findByEmail(userInfo.get("email"));
+		return new KakaoDTO(searchInfo.getId(), searchInfo.getName(), searchInfo.getEmail());
     }
-
 
 }
